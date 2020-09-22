c69a8b9fb90f6018278039dfe91a9ad8d7be5edb
kkellerlbl <kkeller@lbl.gov>
2020-09-22 13:48:10 -0700

    Update bulkS3upload.go

--- bulkS3upload.go
+++ bulkS3upload.go
@@ -85,7 +85,7 @@ func copyWorker(bucket string, url string, accessID string, secretKey string, ss
 		objectPath := stringArray[0] + "/" + stringArray[1] + "/" + stringArray[2] + "/" + stringArray[3]
 		fullPath := rootDir + filePath
 		fileinfo, statErr := os.Stat(fullPath)
-		fileSize := 0
+		fileSize := int64(0)
 		if statErr != nil {
 			log.Printf("error statting file %s",fullPath)
 		} else {
