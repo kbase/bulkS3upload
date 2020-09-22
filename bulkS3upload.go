129154f1e70d7b6ecfafc1bb628af65df176ec29
kkellerlbl <kkeller@lbl.gov>
2020-09-22 13:17:26 -0700

    Update bulkS3upload.go

--- bulkS3upload.go
+++ bulkS3upload.go
@@ -85,7 +85,7 @@ func copyWorker(bucket string, url string, accessID string, secretKey string, ss
 		objectPath := stringArray[0] + "/" + stringArray[1] + "/" + stringArray[2] + "/" + stringArray[3]
 		fullPath := rootDir + filePath
 		fileinfo, err := os.Stat(fullPath)
-		_, err := minioClient.FPutObject(ctx, bucket, objectPath, fullPath, minio.PutObjectOptions{})
+		_, err = minioClient.FPutObject(ctx, bucket, objectPath, fullPath, minio.PutObjectOptions{})
 		if err != nil {
 			log.Printf(err.Error())
 		}
